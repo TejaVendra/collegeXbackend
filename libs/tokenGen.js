c44c86bfcdb1b798541098e501b960a3b82145ea
Teja <tejavitap@gmail.com>
2026-03-30 22:59:09 +0530

    updated 3

--- libs/tokenGen.js
+++ libs/tokenGen.js
@@ -7,8 +7,8 @@ export const generateToken = (userId,res) =>{
     res.cookie("jwt",token,{
         maxAge:1*24*60*60*1000,//ms
         httpOnly:true,
-        sameSite:"strict",
-        secure:process.env.NODE_ENV != "development"
+        sameSite:"none" ,
+        secure:true,
     })
     return token;
 }
